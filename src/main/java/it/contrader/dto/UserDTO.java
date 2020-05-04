a9f5539ab98b9acdce828a9756c40b5cbdd9ef6f
Federico <federicolucido@gmail.com>
2020-05-04 12:23:01 +0200

    Fix iniziale

--- src/main/java/it/contrader/dto/UserDTO.java
+++ src/main/java/it/contrader/dto/UserDTO.java
@@ -1,5 +1,8 @@
 package it.contrader.dto;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
 import it.contrader.model.User.Usertype;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,11 +15,11 @@ import lombok.NoArgsConstructor;
 public class UserDTO {
 
 	private Long id;
-
+	
 	private String username;
 
 	private String password;
-
+	@Enumerated(EnumType.STRING)
 	private Usertype usertype;
 
 }
