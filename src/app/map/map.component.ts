34329e414254292270f9dee52ef3ca138dea8794
Ivan5122 <sao58412@gmail.com>
2020-05-15 10:23:09 +0200

    map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 import { MapService } from 'src/service/map.service';
-import { UserDTO } from 'src/dto/mapdto';
+import { MapDTO } from 'src/dto/mapdto';
 
 @Component({
   selector: 'app-map',
@@ -10,33 +9,37 @@ import { UserDTO } from 'src/dto/mapdto';
 })
 export class MapComponent implements OnInit {
 
-  users: MapDTO[];
-  usertoinsert: MapDTO = new MapDTO();
+  maps: MapDTO[];
+  maptoinsert: MapDTO = new MapDTO(0, 0, "");
 
   constructor(private service: MapService) { }
 
   ngOnInit() {
-    this.getUsers();
+    this.getMaps();
   }
 
-  getUsers() {
-    this.service.getAll().subscribe(users => this.users = users);
+  getMaps() {
+    this.service.getAll().subscribe(maps => this.maps = maps);
+  }
+
+  findByIdFolder(idFolder: number){
+    this.service.getallbyidfolder(idFolder).subscribe(maps => this.maps = maps);
   }
 
   delete(map: MapDTO) {
-    this.service.delete(user.id).subscribe(() => this.getUsers());
+    this.service.delete(map.idMap).subscribe(() => this.getMaps());
   }
 
   update(map: MapDTO) {
-    this.service.update(user).subscribe(() => this.getUsers());
+    this.service.update(map).subscribe(() => this.getMaps());
   }
 
   insert(map: MapDTO) {
-    this.service.insert(user).subscribe(() => this.getUsers());
+    this.service.insert(map).subscribe(() => this.getMaps());
   }
 
   clear(){
-    thistoinsert = new MapDTO();
+    this.maptoinsert = new MapDTO(0, 0, "");
   }
 
 }
