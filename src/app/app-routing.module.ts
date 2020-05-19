7a3b715fd01771095172b708be1dd56813def5f9
Federico <federicolucido@gmail.com>
2020-05-19 09:50:09 +0200

    fix merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-<<<<<<< HEAD
-
-
-
-
-
-
-
-import { MapComponent } from './map/map.component';
-=======
->>>>>>> 561c793d478c007795ca451ad2d0318227e1a8f3
 
 
 /**
@@ -24,22 +13,8 @@ import { MapComponent } from './map/map.component';
  * @author Vittorio Valent
  */
 const routes: Routes = [
-<<<<<<< HEAD
-  { path: '', redirectTo:'/login', pathMatch: 'full' },
-
-
-  
-
-  
-  { path: 'map', component: MapComponent }
-
-
-
-
-=======
   { path: '', redirectTo:'/login', pathMatch: 'full' }
   
->>>>>>> 561c793d478c007795ca451ad2d0318227e1a8f3
 ];
 
 @NgModule({
